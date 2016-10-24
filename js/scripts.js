e8d38698ee1748f9ca2487af89ac990a050d97f8
Annie Nguimzong Sonna <nsab42@yahoo.com>
2016-10-24 13:40:16 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,22 @@
 $(function(){
   $("#fields form").submit(function() {
-    var titleInput = $("input#title").val();
-    var nameInput = $("input#name").val();
-    var positionInput = $("input#position").val();
-    var favCompanyInput = $("input#favCompany").val();
-    var addressInput = $("input#address").val();
+    var inputs = ['title', 'name', 'position', 'favCompany', 'address'];
+    inputs.forEach(function(input) {
+       var userInput = $("input#" + input).val();
+      $("." + input).text(userInput);
+    });
+   
+//     var titleInput = $("input#title").val();
+//     var nameInput = $("input#name").val();
+//     var positionInput = $("input#position").val();
+//     var favCompanyInput = $("input#favCompany").val();
+//     var addressInput = $("input#address").val();
 
-    $(".name").text(nameInput);
-    $(".address").text(addressInput);
-    $(".position").text(positionInput);
-    $(".favCompany").text(favCompanyInput);
-    $(".title").text(titleInput);
+//     $(".name").text(nameInput);
+//     $(".address").text(addressInput);
+//     $(".position").text(positionInput);
+//     $(".favCompany").text(favCompanyInput);
+//     $(".title").text(titleInput);
 
     $("#letter").show();
 
